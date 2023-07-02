5cf953639dd6818dee8915ca53108ed8d1fddaf0
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2023-07-02 06:22:33 +0300

    Todo Adder component

---
+++ vue-todos/src/views/TodosView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import TodoAdder from '../components/TodoAdder.vue';
+
+</script>
+
+<template>
+  <main>
+    <h1>Add Todo</h1>
+    <TodoAdder></TodoAdder>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+</style>
