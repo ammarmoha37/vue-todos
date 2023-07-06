b07298d322fb3022fcc0e33370993dcf13f04ceb
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2023-07-06 06:35:55 +0300

    reuploading files to reorder for deployment

---
+++ src/components/TodoButton.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <button>
+      <slot></slot>
+    </button>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+button {
+  background-color: #dddada;
+  padding: 8px 16px;
+  border: none;
+}
+</style>
