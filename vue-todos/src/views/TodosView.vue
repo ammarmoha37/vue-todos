3cf72cdc7947d673edcc0cc95020b36dd797f26b
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2023-07-06 04:56:28 +0300

    delete todo

--- vue-todos/src/views/TodosView.vue
+++ vue-todos/src/views/TodosView.vue
@@ -25,6 +25,10 @@ const toggleEditTodo = (todoPos) => {
 const updateTodo = (todoVal, todoPos) => {
   todoList.value[todoPos].todo = todoVal;
 }
+
+const deleteTodo = (todoId) => {
+  todoList.value = todoList.value.filter((todo) => todo.id !== todoId);
+}
 </script>
 
 <template>
@@ -39,6 +43,7 @@ const updateTodo = (todoVal, todoPos) => {
         @toggle-complete="toggleTodoComplete"
         @edit-todo="toggleEditTodo"
         @update-todo="updateTodo"
+        @delete-todo="deleteTodo"
       />
     </ul>
     <p class="todos-msg" v-else>
