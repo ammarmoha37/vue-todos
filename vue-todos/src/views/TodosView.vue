17d65102711f41719c6bd93bfda2c02385064950
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2023-07-06 04:03:37 +0300

    todo item component

--- vue-todos/src/views/TodosView.vue
+++ vue-todos/src/views/TodosView.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import { uid } from 'uid';
 import TodoCreator from '../components/TodoCreator.vue';
+import TodoItem from '../components/TodoItem.vue';
 
 const todoList = ref([]);
 
@@ -19,6 +20,12 @@ const createTodo = (todo) => {
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem v-for="todo in todoList" :todo="todo"/>
+    </ul>
+    <p class="todos-msg" v-else>
+      <span>You have no todo's to Complete! Add one!</span>
+    </p>
   </main>
 </template>
 
@@ -34,6 +41,21 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
